3b1307c082e372d77b6e6a5df73ca560a7c723c8
abid-sara <sara.abid@ensia.edu.dz>
2024-11-25 21:39:17 +0100

    added project structure

---
+++ lib/style/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// Define your custom colors here
+class AppColors {
+  static const Color darkGreen = Color.fromRGBO(22, 66, 60, 0);
+  static const Color green = Color.fromRGBO(106, 156, 137, 0);
+  static const Color lightGreen = Color.fromRGBO(196, 218, 210, 0);
+  static const Color yellowGreen = Color(0x00E4EFDF);
+  static const Color purpule = Color.fromRGBO(169, 169, 199, 0);
+}
