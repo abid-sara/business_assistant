e2a52aad0ed7f71053dd3785bde96e78f89f1f12
abid-sara <sara.abid@ensia.edu.dz>
2024-11-28 11:16:40 +0100

    Back Arrow is here in style you can use it directly

---
+++ lib/style/widgets.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../style/colors.dart';
+
+class BackArrow extends StatelessWidget implements PreferredSizeWidget {
+  final VoidCallback? onPressed;
+
+  const BackArrow({super.key, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      automaticallyImplyLeading: false,
+      flexibleSpace: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 23, top: 23),
+            child: Container(
+              padding: const EdgeInsets.all(2), // Padding within the circle
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: AppColors.yellowGreen, // Background color
+              ),
+              child: IconButton(
+                icon: const Icon(
+                  Icons.keyboard_arrow_left,
+                  size: 30,
+                  color: AppColors.darkGreen, // Icon color
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(70);
+}
