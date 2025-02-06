42d747ea2bfa8bff5454c4e3963f58c099831e79
Meriemdemil <120749821+Meriemdemil@users.noreply.github.com>
2025-02-06 12:40:35 +0100

    final auth_state.dart

--- lib/cubits/Authentification/auth_state.dart
+++ lib/cubits/Authentification/auth_state.dart
@@ -4,12 +4,22 @@ class AuthInitial extends AuthState {}
 
 class AuthLoading extends AuthState {}
 
-class AuthAuthenticated extends AuthState {}
+class AuthAuthenticated extends AuthState {
+  final String? userName = 'Guest'; 
+  AuthAuthenticated();
+}
+
+class AuthUserNameFetched extends AuthState {
+  final String userName;
+
+  AuthUserNameFetched(this.userName);
+}
 
 class AuthError extends AuthState {
   final String message;
   AuthError(this.message);
 }
+
 
 class AuthLinkPressedState extends AuthState {
   final bool isLinkPressed;
